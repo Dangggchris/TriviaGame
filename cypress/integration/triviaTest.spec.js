bd48c7998c7c449373051dc92f85fc67d8457c0d
Dangggchris <christophermtdang@gmail.com>
2020-05-27 15:25:31 -0700

    testing scripts with cypress

---
+++ cypress/integration/triviaTest.spec.js
@@ -0,0 +1,10 @@
+describe('My First Test', () => {
+  it('Navigate to Trivia Site', () => {
+    cy.visit('https://dangggchris.github.io/TriviaGame/')
+  })
+
+  it('Start game', () => {
+    cy.get('#start').click()
+  })
+
+})
